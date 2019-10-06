6906911d38881fcee871c3d1f0702e2ba88ab720
Pavel <pavel85_07@bk.ru>
2019-10-06 12:52:49 +0300

    Сладйер превью - другой подход

--- src/js/main.js
+++ src/js/main.js
@@ -2,56 +2,37 @@ let line = document.querySelector('.line');
 let images = document.querySelectorAll('.img-gallary');
 let sliderWidth = document.querySelector('.wrap-img').offsetWidth;
 let widthArray = [0];
+let imageWidth;
 let lineWidth = 0;
 let offset = 0;
 let step = 0;
-let residue;
+let residue = 0;
 let next = document.querySelector('.arrow-right');
 let prev = document.querySelector('.arrow-left');
+let lineLeft = 0;
+
 
 
 for (let i = 0; i < images.length; i++){
     widthArray.push(images[i].offsetWidth);
     lineWidth += images[i].offsetWidth + parseInt(getComputedStyle(images[i]).marginRight);
+    imageWidth = images[0].offsetWidth + parseInt(getComputedStyle(images[i]).marginRight);
 }
-console.log(lineWidth);
-line.style.width = lineWidth+'px';
 
+
+line.style.width = lineWidth+'px';
 
 next.onclick = function () {
 
-    residue = lineWidth - sliderWidth - (offset + widthArray[step]);
+    residue = lineWidth - sliderWidth + imageWidth;
 
-    if(residue >= 0){
-        if(offset == 0){
-            step = 1;
-            offset = 1
-        }
-        offset = offset + widthArray[step];
-        line.style.left = -offset+'px';
-        console.log(offset, step);
-        console.log('равно 0');
-    }
-    else {
+    offset = offset + imageWidth;
+    line.style.left = -offset+'px';
+    console.log(offset, step, residue);
+
+    if(offset > residue){
         offset = 0;
         step = 0;
-        line.style.left = -(lineWidth - sliderWidth)+'px';
-        console.log(offset, step);
-        console.log('больше 0');
-
-    }
-
-    if(step+1 == images.length){
-        step = 0;
-        offset = 0;
-        line.style.left = 0;
-        console.log(offset, step);
-        console.log('условия равны');
-    }
-    else {
-        step++;
-        console.log(offset, step);
-        console.log('мимо условий');
     }
 
 
@@ -59,11 +40,5 @@ next.onclick = function () {
 
 prev.onclick = function () {
     step--;
-    let currentLeft = parseInt(getComputedStyle(line).left);
-    line.style.left = (currentLeft + widthArray[step])+'px';
-
-    console.log(offset, step);
-
-
-
+    line.style.left = + offset+'px';
 }
